f59bfee6505420ae548cb7c0055763a65dc53752
Onkar-code <cf19onkar.singh@iesjoandaustria.org>
2020-02-22 16:57:42 +0100

    pruebas de integración

--- ComandaNou.java
+++ ComandaNou.java
@@ -4,21 +4,14 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 public class ComandaNou {
     
-    final static int TAMANY_MAXIM_FILES = 99;
-    final static int TAMANY_MAXIM_COLUMNES = 99;
+    //final static int TAMANY_MAXIM_FILES = 99;
+    //final static int TAMANY_MAXIM_COLUMNES = 99;
     
-    final static int TAMANY_MINIM_FILES= 1;
-    final static int TAMANY_MINIM_COLUMNES= 1;    
+    //final static int TAMANY_MINIM_FILES= 1;
+    //final static int TAMANY_MINIM_COLUMNES= 1;    
     
-    static boolean taulellEnConstruccio = false;
+    //static boolean taulellEnConstruccio = true;
     
-    /*
-    //comproba si hi ha algún taulell en construccio
-    static boolean taulellEnConstruccio(){
-        //TODO el cas on hi ha un en construcció
-        return false;
-    }
-    */
     // comprobar si les dimensions son  válides
     static boolean dimensionsValida(int valor) {
         if ((valor < TAMANY_MINIM_FILES || valor > TAMANY_MAXIM_FILES) || (valor < TAMANY_MINIM_COLUMNES || valor > TAMANY_MAXIM_COLUMNES)) {
@@ -31,17 +24,15 @@ public class ComandaNou {
         }             
     }
     
-    public static void main(String[] args) throws IOException{
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        int filesEnter, columnaEnter;
-        
+    static void nou(){
         //TODO si hi ha un vaixell considerar la opció GUARAD o OBLIDA
         if (! taulellEnConstruccio) {
         
             //demanem numero de columnes del taulell
             System.out.println("Introdueix número de files del taulell:");
             
-            //pendiente control de flujo de entrada, suponemos que recibimos siempre digitos        
+            //pendiente control de flujo de entrada
+            //suponemos que recibimos siempre digitos        
             do {
                 String files = entrada.readLine();
                 filesEnter = Integer.parseInt(files);
@@ -56,11 +47,28 @@ public class ComandaNou {
                 columnaEnter = Integer.parseInt(columna);
             
             } while (! dimensionsValida(columnaEnter));
-            
+        
+        //caso en el que existeix un taulell 
         } else {
             //TODO
-            System.out.println("TODO, opciones GUARDAR o OBLIDA");
+            System.out.println("Ja hi ha un taulell. Considereu les opcions GUARDA o OBLIDA");
+            String comanda;
+            String[] llistaComandes = {"GUARDA","OBLIDA"};
+            
+            do {                
+                comanda = entrada.readLine();            
+            } while (! ConstructorTaulell.comandaEsValida(comanda, llistaComandes));
+            
+            System.out.println("OK");
         } 
+    
+    
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        int filesEnter, columnaEnter;
+        //cridem a la funció nou
+        nou();                 
     }       
 }
            
