b22050c0fe75a857b61a2a385eaed5493b536c8b
AlejandroRO <cf19alejandro.ramos@iesjoandaustria.org>
2020-02-22 11:06:21 +0100

    menu inicial

--- ConstructorTaulell.java
+++ ConstructorTaulell.java
@@ -1,3 +1,10 @@
+
+//Aquestes llibreries són necessaries per l'entrada de dades de l'usuari (BufferedReader)
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
 public class ConstructorTaulell{
 
     // Definim les variables globals:
@@ -6,12 +13,22 @@ public class ConstructorTaulell{
     final static int TAMANY_MAXIM_COLUMNES = 99;
 
     //main
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
-        System.out.println(TAMANY_MAXIM_COLUMNES);
-        System.out.println(TAMANY_MAXIM_FILES);
+        System.out.println("Benvingut a Enfonsar la flota \nAra hi ets al Constructor de taulells");
+        System.out.println("Introdueix la comanda que vols executar: ");
+
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        String comanda = entrada.readLine();       // llegim una línia de text
+
+
+
 
         
     }
+
+    //public static void menuAjuda{
+
+    //}
 
 }
