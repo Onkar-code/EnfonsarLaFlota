4347ab82f0537b93d0458f6bb3fd43aff462dc7f
Onkar-code <cf19onkar.singh@iesjoandaustria.org>
2020-02-22 10:45:40 +0100

    proba, pujem sense class

--- ConstructorTaulell.java
+++ ConstructorTaulell.java
@@ -5,7 +5,7 @@ public class ConstructorTaulell{
     final static int TAMANY_MAXIM_FILES = 99;
     final static int TAMANY_MAXIM_COLUMNES = 99;
 
-
+    //main
     public static void main(String[] args){
 
         System.out.println(TAMANY_MAXIM_COLUMNES);
