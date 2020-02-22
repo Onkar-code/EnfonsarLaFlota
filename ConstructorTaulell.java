dab2ab7e8bcd929066732cc431424aa827df83bd
AlejandroRO <cf19alejandro.ramos@iesjoandaustria.org>
2020-02-22 10:35:18 +0100

    primer commit

---
+++ ConstructorTaulell.java
@@ -0,0 +1,17 @@
+public class ConstructorTaulell{
+
+    // Definim les variables globals:
+    // Aquestes dues inidquen el tamany màxim del taulell, són constants.
+    final static int TAMANY_MAXIM_FILES = 99;
+    final static int TAMANY_MAXIM_COLUMNES = 99;
+
+
+    public static void main(String[] args){
+
+        System.out.println(TAMANY_MAXIM_COLUMNES);
+        System.out.println(TAMANY_MAXIM_FILES);
+
+        
+    }
+
+}
