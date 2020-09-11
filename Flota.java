5b0a1e4e3262b945374be8f5357813f8255f64b4
AlejandroRO <cf19alejandro.ramos@iesjoandaustria.org>
2020-09-11 22:43:50 +0200

    queda pendiente juga()

--- Flota.java
+++ Flota.java
@@ -4,9 +4,14 @@ import java.io.InputStreamReader;
 
 // implementació del joc
 public class Flota{
+    static int[][] taulellOriginal;
     static int[][] taulell;
+    static Boolean[][] taulellBoolea;
+
+    static String[] llistaComandes = {"SURT","SOLUCIONA","AJUDA","JUGA"};
     static int files;
     static int columnes;
+
     public static void main(String[] args) throws IOException{
         /*
         En arrencar el programa, carrega el taulell contingut a un fitxer, a partir del nom del taulell segons s’hagi definit a ConstructorTaulell.
@@ -14,22 +19,72 @@ public class Flota{
         De no trobar-se el taulell amb aquest nom, finalitza execució amb un missatge d’error.
         El programa valida que el contingut del fitxer sigui correcte. De no ser-ho, finalitza execució amb un missatge d’error.
         */
-        System.out.println("Nom taulell? ");
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        String nomTaulell = entrada.readLine();
+        System.out.println("Benvingut a Enfonsar la flota \nAra hi ets al Joc principal.");
+        System.out.println("Introdueix el nom del taulell que vols carregar: ");
+        BufferedReader entradaTaulell = new BufferedReader(new InputStreamReader(System.in));
+        String nomTaulell = entradaTaulell.readLine();
         
-        taulell = Fitxers.carregaTaulell(nomTaulell);
+        taulellOriginal = Fitxers.carregaTaulell(nomTaulell);
 
-        if(taulell != null){
-            files = ConstructorTaulell.obteFilesTaulell(taulell);
-            columnes = ConstructorTaulell.obteColumnesTaulell(taulell);
+        if(taulellOriginal != null){
+            taulell = TaulellJoc.creaTaulell(taulellOriginal);
+            taulellBoolea = TaulellJoc.creaTaulellBoolea(taulellOriginal);
+            TaulellJoc.mostraTaulell(taulell, taulellBoolea);
         }
+       
+        String comanda;
+
+        // Amb aquest do()while{} el que demanem és l'entrada de la comanda per part de l'usuari fins que entri una comanda vàlida.
+        do{
+            
+            System.out.println("Introdueix la comanda que vols executar ( escriu 'AJUDA' per veure les comandes ): ");
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            comanda = entrada.readLine();       // llegim una línia de text
+
+            //si l'entrada és vàlida llavors és processarà amb la funció gestorComanda
+            if(ConstructorTaulell.comandaEsValida(comanda, llistaComandes)){
+                gestorComanda(comanda);
+            }else{
+                System.out.println("Comanda no vàlida. Torna a intentar-ho ( escriu 'AJUDA' per veure les comandes )");
+            }
+
+        }
+        while (! comanda.toUpperCase().equals("SURT"));
+
 
         /*
         D’estar tot correcte, el programa mostra el mapa del taulell on, inicialment totes les cel·les apareixen amb el valor ocult sota un símbol d’interrogació.
         Com a ajuda, al voltant dels valors del taulell, apareixeran els noms de la fila/la columna corresponent.
         */
-        TaulellJoc.mostraTaulell(taulell);
+        TaulellJoc.mostraTaulell(taulellBoolea);
         //TaulellJoc.mostraTaulell();
+    }
+
+        public static void gestorComanda(String comanda){
+
+        // Si l'entrada és vàlida la convertim en majúscules
+        comanda = comanda.toUpperCase();
+
+        switch(comanda){
+            case "AJUDA":
+                TaulellJoc.ajuda();
+                break;
+
+            case "SOLUCIONA":
+                TaulellJoc.soluciona(taulellOriginal);
+                break;
+
+            case "JUGA":
+                TaulellJoc.juga();
+                break;
+            
+            case "SURT":
+                ConstructorTaulell.surt();
+                break;
+            
+            // en principi el default no s'executarà mai. Podriem tancar el programa si s'executa aquest codi.
+            default: System.out.println("Comanda desconeguda, estem al final del switch");
+        }
     }
 }
--- TaulellJoc.java
+++ TaulellJoc.java
@@ -1,13 +1,56 @@
 import java.util.Arrays;
 
+// import per al diccionari de les descripciones d'AJUDA
+import java.util.Hashtable;
+import java.util.Set;
+
 public class TaulellJoc {
-    public static int[][] creaTaulell(int [][] taulellOriginal){
+    public static int[][] creaTaulell(int[][] taulellOriginal){
 
         int[][] copy = Arrays.stream(taulellOriginal).map(int[]::clone).toArray(int[][]::new);
 
         return copy;
     }
-    public static void mostraTaulell(int[][] taulell){
+
+    public static Boolean[][] creaTaulellBoolea(int[][] taulell){
+
+        Boolean[][] taulellBoolea = new Boolean[taulell.length][taulell[0].length];
+
+        for(int i = 0; i < taulell.length ;i++){
+            for(int j = 0; j < taulell[0].length ;j++){
+                    // Inicialitzem tots els valors a false
+                    taulellBoolea[i][j] = false;
+            }
+        }
+
+        return taulellBoolea;
+    }
+
+
+    public static void ajuda(){
+        //el programa mostra un text d’ajuda amb les diferents comandes disponibles.
+
+        System.out.println("Les comandes que pots utilizar són:");
+
+        //Descripció comandes com a hashtable (diccionari), s'utiliza amb la comanda AJUDA
+        Hashtable<String, String> descripcioComandes = new Hashtable<String, String>();
+        descripcioComandes.put("AJUDA","Mostra les diferents comandes disponibles.");
+        descripcioComandes.put("SOLUCIONA","Resol el taulell. Hi perdràs!");
+        descripcioComandes.put("JUGA","Introdueix les coordenades del vaixell a esbrinar. Format (y0:y1, x0:x1)");
+        descripcioComandes.put("SURT","Finalitza el joc.");
+
+        // Obtinc totes les claus de descripcioComanes perquè vull recorrer el diccionari.
+        Set<String> keysDescripcioComandes = descripcioComandes.keySet();
+
+        //Recorrem les descripcions
+        for(String key : keysDescripcioComandes){
+            System.out.println( key + ": " + descripcioComandes.get(key));
+        }
+        System.out.println();
+
+    }
+
+    public static void mostraTaulell(int[][] taulell, Boolean[][] taulellBoolea){
 
         int files = ConstructorTaulell.obteFilesTaulell(taulell);
         int columnes = ConstructorTaulell.obteColumnesTaulell(taulell);
@@ -29,7 +72,53 @@ public class TaulellJoc {
         // El mostrem per pantalla
         for(int i = 0; i < files ;i++){
             for(int j = 0; j < columnes ;j++){
+                if(taulellBoolea[i][j] == true){
+                    System.out.print(taulell[i][j] + " ");
+                }else{
+                    System.out.print("? ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void mostraTaulell(Boolean[][] taulell){
+
+        // Crearem el taulell que és una matriu de strings a partir del taulell numeric
+        String[][] taulellString = new String[taulell.length][taulell[0].length];
+
+        // El mostrem per pantalla
+        for(int i = 0; i < taulell.length ;i++){
+            for(int j = 0; j < taulell[0].length ;j++){
                 System.out.print(taulell[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void soluciona(int[][] taulell){
+
+        int files = ConstructorTaulell.obteFilesTaulell(taulell);
+        int columnes = ConstructorTaulell.obteColumnesTaulell(taulell);
+
+        // Crearem el taulell que és una matriu de strings a partir del taulell numeric
+        String[][] taulellString = new String[files][columnes];
+    
+        for(int i = 0; i < files ;i++){
+            for(int j = 0; j < columnes ;j++){
+                if(taulell[i][j] == 0){
+                    // El valor 0 corresponent a l'aigua és canvia pel "."
+                    taulellString[i][j] = ".";
+                }else{
+                    taulellString[i][j] = Integer.toString(taulell[i][j]);
+                }
+            }
+        }
+
+        // El mostrem per pantalla
+        for(int i = 0; i < files ;i++){
+            for(int j = 0; j < columnes ;j++){
+                    System.out.print(taulell[i][j] + " ");
             }
             System.out.println();
         }
